bc427de30b7284545767d0860a08002895a903d4
mathistrunet <mathtrunet101@gmail.com>
2026-01-04 16:57:41 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173
+  }
+});
+
+export default defineConfig({
   plugins: [react()],
 });
